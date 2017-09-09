00287fdd963f411956a9b848d5775135dfa48303
Fredrik <fredrik.wallstrom.1992@gmail.com>
2017-09-09 10:21:59 +0200

    Started working on the sorting algoritms

--- Lab2/e2_1.pl
+++ Lab2/e2_1.pl
@@ -1,8 +1,22 @@
 % Exercise 2.1
 
 sorted([X1, X2]) :-
-	X1<X2.
+	X1<=X2.
 
 sorted([X1, X2 | List]) :-
-	X1<X2,
+	X1<=X2,
 	sorted([X2 | List]).
+
+ssort(L, []) :-
+	sorted(L).
+
+ssort(L, LS) :-
+	min_list(L, Min),
+	delete(L, Min, L1),
+	ssort(L1, [Min|LS]).
+
+qsort([X1 | L], LS) :-
+	split(L, X1, L1, L2),
+	qsort(L1),
+	qsort(L2),
+	append(L1, L2, LS).

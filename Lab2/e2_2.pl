02f8a026526d366997298ece22a04ba76b1baeae
Fredrik Wallström <frewa814@student.liu.se>
2017-09-17 13:04:16 +0200

    Removed some white lines

--- Lab2/e2_2.pl
+++ Lab2/e2_2.pl
@@ -2,14 +2,10 @@
 
 % middle(X,Xs)
 % X is the middle element in the list Xs
-
 middle(X, [First|Xs]) :-
 	append(Middle, [Last], Xs),
 	middle(X, Middle).
 middle(X, [X]).
-	
-	
-
 	
 /*
 Case 1 - Order:

91fc7dbf68fb960ffabb08f193ab23425c11412d
Fredrik Wallström <frewa814@student.liu.se>
2017-09-23 20:00:55 +0200

    Lab 3 done

--- Lab3/e3_1.pl
+++ Lab3/e3_1.pl
@@ -1,37 +1,85 @@
 % Exercise 3.1
 
+% run(In, String, Out).
+% If String is a string of commands and
+% In is a initial binding environment (represented as [[Id1, Num1], [Id2, Num2], ...]).
+% Than Out is the final binding environment.
 run(In, String, Out) :-
 	scan(String, Tokens),
-	parse(Tokens, AbstStx).
+	parse(Tokens, AbstStx),
+	execute(In, AbstStx, Out).
 
+% parse(Tokens, AbstStx).
+% If Tokens is a list of tokens,
+% AbstStx is a term that can be executed by the interpreter (written in Exercise 2.3).
 parse(Tokens, AbstStx) :-
 	pgm(AbstStx, Tokens, []).
 
-
+% pgm(X).
+% DCG parser that recognizes if there is one program X, or a sequence of programs X and Y.
 pgm(X) --> cmd(X).
 pgm(seq(X,Y)) --> cmd(X), [;], pgm(Y).
 
+% cmd(X).
+% DCG parser that recognizes the commands (skip, set, if, or while) in the program.
 cmd(skip) --> [skip].
 cmd(set(X, Y)) --> [X], [:=], expr(Y).
 cmd(if(Bool, Pgm1, Pgm2)) --> [if], bool(Bool), [then], pgm(Pgm1), [else], pgm(Pgm2), [fi].
 cmd(while(Bool, Pgm)) --> [while], bool(Bool), [do], pgm(Pgm), [od].
 
-bool(X < Y) --> expr(X) < expr(Y).
-bool(X =< Y) --> expr(X) =< expr(Y).
-bool(X == Y) --> expr(X) == expr(Y).
-bool(X > Y) --> expr(X) > expr(Y).
-bool(X >= Y) --> expr(X) >= expr(Y).
+% bool(X).
+% DCG parser that recognizes the boolean expressions in the program.
+bool(X < Y) --> expr(X), [<], expr(Y).
+bool(X =< Y) --> expr(X), [=<], expr(Y).
+bool(X == Y) --> expr(X), [==], expr(Y).
+bool(X > Y) --> expr(X), [>], expr(Y).
+bool(X >= Y) --> expr(X), [>=], expr(Y).
 
-expr(X * Y) --> factor(X) * expr(Y).
+% expr(X).
+% DCG parser that recognizes the arithmetic expression (*) in the program.
+expr(X * Y) --> factor(X), [*], expr(Y).
 expr(X) --> factor(X).
 
-factor(X + Y) --> term(X) + factor(Y).
+% factor(X).
+% DCG parser that recognizes the arithmetic expression (+) in the program.
+factor(X + Y) --> term(X), [+], factor(Y).
 factor(X) --> term(X).
 
+% term(X).
+% DCG parser that recognizes the term in the program.
 term(X) --> [X].
 
+/* Example queries for the program are listed below
 
-	
+% Set y=1.
+| ?- run([], "y:=1", X).
+X = [[y,1]] ? ;
+no
+
+% Change y=1 to y=2.
+| ?- run([[y,1]], "y:=2", X).
+X = [[y,2]] ? ;
+no
+
+% Add x and y, answer is z.
+| ?- run([[y,1], [x,9]], "z:=x+y", X).
+X = [[y,1],[x,9],[z,10]] ? ;
+no
+
+% Add x and y if x is greater than y (answer is z), else set x and y to 0.
+| ?- run([[y,1], [x,9]], "if x>y then z:=x+y else x:=0; y:=0 fi", X).
+X = [[y,1],[x,9],[z,10]] ? ;
+no
+| ?- run([[y,10], [x,9]], "if x>y then z:=x+y else x:=0; y:=0 fi", X).
+X = [[x,0],[y,0]] ? ;
+no
+
+% Factorial of x, in this case x=4. Factorial of 4 is y=24.
+| ?-  run([[x,4]], "y:=1; z:=0; while x>z do z:=z+1; y:=y*z od", X).
+X = [[x,4],[z,4],[y,24]] ? ;
+no
+
+*/
 
 % Scanner for assignment 3
 % TDDD08 Logic Programming

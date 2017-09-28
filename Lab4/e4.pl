bf4c6ee4c124fb0c751f8213413a77c6f38c29ed
Fredrik Wallström <frewa814@student.liu.se>
2017-09-28 22:28:37 +0200

    Searches now working, need to add running examples and maybe the results need to be printed in a proper readable way

--- Lab4/e4.pl
+++ Lab4/e4.pl
@@ -1,102 +1,94 @@
-init(state(3,3,0,0)).
-goal(state(0,0,3,3)).
+% Exercise 4
+% Fredrik Wallström
 
-% Move one Cannibal, Cannibal rower.
-action(state(CannibalLeft, MissionaryLeft, CannibalRight, MissionaryRight),
-       state(CannibalLeftNew, MissionaryLeft, CannibalRightNew, MissionaryRight)) :-
-	CannibalLeftNewTmp is CannibalLeft - 1,
-	CannibalRightNewTmp is CannibalRight + 1,
-	
-	CannibalLeftNewTmp >= 0,
-	CannibalLeftNewTmp =<  MissionaryLeft,
+% Set prolog flag to print the whole result and not just a part of a list.
+% set_prolog_flag(toplevel_print_options, [quoted(true), portrayed(true), max_depth(0)]).
 
-	CannibalRightNewTmp =< 3,
-	(CannibalRightNewTmp =< MissionaryRight ; MissionaryRight == 0),
-	
-	CannibalLeftNew is CannibalLeft - 1,
-	CannibalRightNew is CannibalRight + 1.
+% Initialize the start and the goal state of the problem.
+% One state is represented as:
+% state(#CannibalsLeft, #MissionariesLeft, #CannibalsRight, #MissionariesRight, Boatside).
+init(state(3, 3, 0, 0, left)).
+goal(state(0, 0, 3, 3, right)).
 
-% Move one Missionary, Cannibal rower.
-action(state(CannibalLeft, MissionaryLeft, CannibalRight, MissionaryRight),
-       state(CannibalLeft, MissionaryLeftNew, CannibalRight, MissionaryRightNew)) :-
-	MissionaryLeftNewTmp is MissionaryLeft - 1,
-	MissionaryRightNewTmp is MissionaryRight + 1,
-	
-	MissionaryLeftNewTmp >= 0,
-	MissionaryLeftNewTmp =<  CannibalLeft,
-	
-	MissionaryRightNewTmp >=  CannibalRight,
-	MissionaryRightNewTmp =< 3,
-	
-	CannibalLeftNew is CannibalLeft - 1,
-	CannibalRightNew is CannibalRight + 1.
+%------------------------ PROBLEM ------------------------%
 
-% Move one Cannibal, Missionary rower.
-
-% Move one Missionary, Missionary rower.
-
-
-
-% Move two cannibals
-action(state(CL, ML, CR, MR), state(CL1, ML, CR1, MR)) :-
-	Tmp1 is CL - 2,
-	Tmp1 >= 0,
-	Tmp1 =< ML,
-	Tmp2 is CR + 2,
-	Tmp2 =< 3,
-	(Tmp2 =< MR ; MR == 0),
+% Move two cannibals, left to right.
+action(state(CL, ML, CR, MR, left), state(CL1, ML, CR1, MR, right)) :-
 	CL1 is CL - 2,
-	CR1 is CR + 2.
+	CR1 is CR + 2,
+	state_allowable(CL1, ML, CR1, MR).
 
-% Move one cannibal
-action(state(CL, ML, CR, MR), state(CL1, ML, CR1, MR)) :-
-	Tmp1 is CL - 1,
-	Tmp1 >= 0,
-	Tmp1 =< ML,
-	Tmp2 is CR + 1,
-	Tmp2 =< 3,
-	(Tmp2 =< MR ; MR == 0),
-	CL1 is CL - 1,
-	CR1 is CR + 1.
-
-% Move two missionary
-action(state(CL, ML, CR, MR), state(CL, ML1, CR, MR1)) :-
-	Tmp1 is ML - 2,
-	Tmp2 is MR + 2,
-	Tmp1 >= 0,
-	Tmp2 =< 3,
-	Tmp1 >= CL,
-	Tmp2 >= CR,
+% Move two missionaries, left to right.
+action(state(CL, ML, CR, MR, left), state(CL, ML1, CR, MR1, right)) :-
 	ML1 is ML - 2,
-	MR1 is MR + 2.
+	MR1 is MR + 2,
+	state_allowable(CL, ML1, CR, MR1).
 
-% Move one missionary
-action(state(CL, ML, CR, MR), state(CL, ML1, CR, MR1)) :-
-	Tmp1 is ML - 1,
-	Tmp2 is MR + 1,
-	Tmp1 >= 0,
-	Tmp2 =< 3,
-	Tmp1 >= CL,
-	Tmp2 >= CR,
-	ML1 is ML - 1,
-	MR1 is MR + 1.
-
-% Move one missionary, one cannibal
-action(state(CL, ML, CR, MR), state(CL1, ML1, CR1, MR1)) :-
-	Tmp1 is ML - 1,
-	Tmp2 is MR + 1,
-	Tmp3 is CL - 1,
-	Tmp4 is CR + 1,
-	Tmp3 >= 0,
-	Tmp1 >= 0,
-	Tmp2 =< 3,
-	Tmp4 =< 3,
-	Tmp1 >= Tmp3,
-	Tmp2 >= Tmp4,
+% Move one missionary and one cannibal, left to right.
+action(state(CL, ML, CR, MR, left), state(CL1, ML1, CR1, CR1, right)) :-
 	ML1 is ML - 1,
 	MR1 is MR + 1,
 	CL1 is CL - 1,
-	CR1 is CR + 1.
+	CR1 is CR + 1,
+	state_allowable(CL1, ML1, CR1, MR1).
+
+% Move one cannibal, left to right.
+action(state(CL, ML, CR, MR, left), state(CL1, ML, CR1, MR, right)) :-
+	CL1 is CL - 1,
+	CR1 is CR + 1,
+	state_allowable(CL1, ML, CR1, MR).
+
+% Move one missionary , left to right.
+action(state(CL, ML, CR, MR, left), state(CL, ML1, CR, MR1, right)) :-
+	ML1 is ML - 1,
+	MR1 is MR + 1,
+	state_allowable(CL, ML1, CR, MR1).
+
+% Move two cannibals, right to left.
+action(state(CL, ML, CR, MR, right), state(CL1, ML, CR1, MR, left)) :-
+	CL1 is CL + 2,
+	CR1 is CR - 2,
+	state_allowable(CL1, ML, CR1, MR).
+
+% Move two missionaries, right to left.
+action(state(CL, ML, CR, MR, right), state(CL, ML1, CR, MR1, left)) :-
+	ML1 is ML + 2,
+	MR1 is MR - 2,
+	state_allowable(CL, ML1, CR, MR1).
+
+% Move one missionary and one cannibal, right to left.
+action(state(CL, ML, CR, MR, right), state(CL1, ML1, CR1, MR1, left)) :-
+	ML1 is ML + 1,
+	MR1 is MR - 1,
+	ML1 is CL + 1,
+	MR1 is CR - 1,
+	state_allowable(CL1, ML1, CR1, MR1).
+
+% Move one cannibal, right to left.
+action(state(CL, ML, CR, MR, right), state(CL1, ML, CR1, MR, left)) :-
+	CL1 is CL + 1,
+	CR1 is CR - 1,
+	state_allowable(CL1, ML, CR1, MR).
+
+% Move one missionary, right to left.
+action(state(CL, ML, CR, MR, right), state(CL, ML1, CR, MR1, left)) :-
+	ML1 is ML + 1,
+	MR1 is MR - 1,
+	state_allowable(CL, ML1,  CR, MR1).
+
+% Check if the current state is OK!
+% A state is OK, if missionaries is greater than the cannibals on both sides,
+% or if there is none missionary on some side of the river.
+state_allowable(CL, ML, CR, MR) :-
+	CL >= 0,
+	ML >= 0,
+	CR >= 0,
+	MR >= 0,
+	(ML >= CL ; !, ML = 0),
+	(MR >= CR ; !, MR = 0).
+
+
+%------------------------ DEPTH-FIRST SEARCH ------------------------%
 
 % Depth-first search
 df_search(Path) :-
@@ -109,17 +101,41 @@ df_search([S1|Visited], Path) :-
 	nonmember(S2, [S1|Visited]),
 	df_search([S2, S1|Visited], Path).
 
-
-% Breath-first search
+% Breadth-first search
+% Path is a path from start to goal.
 bf_search(Path) :-
 	init(S0),
-	bf_search([[S0]], Path).
-bf_search([[S|Path]|_], [S|Path]) :-
+	bf_search([[S0]], [], Path).
+
+% bf_maze(Paths, FinalPath).
+% Paths is a list of candidate branches, FinalPath is the solution.
+bf_search([[S|Path]|_], _, [S|Path]) :-
 	goal(S).
-bf_search([[S1|Path]|Partials], FinalPath) :-
+bf_search([[S1|Path]|Partials], Visited, FinalPath) :-
+	append([S1], Visited, NewVisited),
 	findall(S2, action(S1, S2), NewStates),
-	expand([S1|Path], NewStates, NewPaths),
+	remove_visited(NewStates, NewVisited, NewFilteredStates),
+	expand([S1|Path], NewFilteredStates, NewPaths),
 	append(Partials, NewPaths, NewPartials),
-	bf_search(NewPartials, FinalPath).
+	bf_search(NewPartials, NewVisited, FinalPath).
+
+% expand(L1. L2, L3).
+% L3 is the expanded list given by expand L1 with L2.
 expand(L1, L2, L3) :-
 	findall([X|L1], member(X,L2), L3).
+
+% Remove visited nodes in breadth first search.
+remove_visited(NewStates, NewVisited, NewFilteredStates) :-
+	subtract(NewStates, NewVisited, NewFilteredStates).
+
+% subtract(L1, L2, L3).
+% If L1 and L2 are lists, than L3 is the set difference: 
+% L1\L2 (i.e, L1 minus L2).
+subtract([], _, []).
+subtract([H|T], L2, L3) :-
+	member(H, L2),
+	!,
+	subtract(T, L2, L3).
+subtract([H|T], L2, [H|T1]) :-
+	subtract(T, L2, T1).
+

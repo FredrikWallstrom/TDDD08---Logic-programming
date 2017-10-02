618e467ae36dfd15ff59df44f549c0beca25a866
Fredrik Wallström <frewa814@student.liu.se>
2017-10-02 09:33:42 +0200

    Added some running examples, ready to turn in

--- Lab4/e4.pl
+++ Lab4/e4.pl
@@ -60,8 +60,8 @@ action(state(CL, ML, CR, MR, right), state(CL, ML1, CR, MR1, left)) :-
 action(state(CL, ML, CR, MR, right), state(CL1, ML1, CR1, MR1, left)) :-
 	ML1 is ML + 1,
 	MR1 is MR - 1,
-	ML1 is CL + 1,
-	MR1 is CR - 1,
+	CL1 is CL + 1,
+	CR1 is CR - 1,
 	state_allowable(CL1, ML1, CR1, MR1).
 
 % Move one cannibal, right to left.
@@ -90,10 +90,11 @@ state_allowable(CL, ML, CR, MR) :-
 
 %------------------------ DEPTH-FIRST SEARCH ------------------------%
 
-% Depth-first search
+% Path is a path from start to goal.
 df_search(Path) :-
 	init(S0),
-	df_search([S0], Path).
+	df_search([S0], UnreversedPath),
+	reverse_list(UnreversedPath, Path, []).
 df_search([S|Visited], [S|Visited]) :-
 	goal(S).
 df_search([S1|Visited], Path) :-
@@ -101,14 +102,15 @@ df_search([S1|Visited], Path) :-
 	nonmember(S2, [S1|Visited]),
 	df_search([S2, S1|Visited], Path).
 
-% Breadth-first search
+%------------------------ BREADTH-FIRST SEARCH ------------------------%
+
 % Path is a path from start to goal.
 bf_search(Path) :-
 	init(S0),
-	bf_search([[S0]], [], Path).
-%	reverse_list(UnreversedPath, Path, []).
+	bf_search([[S0]], [], UnreversedPath),
+	reverse_list(UnreversedPath, Path, []).
 
-% bf_maze(Paths, FinalPath).
+% bf_search(Paths, FinalPath).
 % Paths is a list of candidate branches, FinalPath is the solution.
 bf_search([[S|Path]|_], _, [S|Path]) :-
 	goal(S).
@@ -120,7 +122,7 @@ bf_search([[S1|Path]|Partials], Visited, FinalPath) :-
 	append(Partials, NewPaths, NewPartials),
 	bf_search(NewPartials, NewVisited, FinalPath).
 
-% expand(L1. L2, L3).
+% expand(L1, L2, L3).
 % L3 is the expanded list given by expand L1 with L2.
 expand(L1, L2, L3) :-
 	findall([X|L1], member(X,L2), L3).
@@ -145,3 +147,41 @@ subtract([H|T], L2, [H|T1]) :-
 reverse_list([], Res, Res).
 reverse_list([H|T], Res, L) :-
 	reverse_list(T, Res, [H|L]).
+
+%------------------------ RUNNING EXAMPLES ------------------------%
+/*
+
+%%%%%%%%%% P in the examples can be explained as following.
+%%%%%%%%%% The first element will represent the initialize state of the
+%%%%%%%%%% cannibals and the missionaries, 3 cannibals at left side as first
+%%%%%%%%%% element, 3 missionaries at left side as second element,
+%%%%%%%%%% 0 cannibals at right side as third element, 0 missionaries at
+%%%%%%%%%% right side as fourth element and the boat is at the left side.
+%%%%%%%%%% The second element in P is the state that we can go from the
+%%%%%%%%%% first state, and so on...
+%%%%%%%%%% We got four ways we can transport the cannibals and missionaries
+%%%%%%%%%% over to the right side without having the cannibals eating the
+%%%%%%%%%% missionaries.
+
+  
+| ?- set_prolog_flag(toplevel_print_options, [quoted(true), portrayed(true), max_depth(0)]).
+yes
+| ?- df_search(P).
+P = [state(3,3,0,0,left),state(1,3,2,0,right),state(2,3,1,0,left),state(0,3,3,0,right),state(1,3,2,0,left),state(1,1,2,2,right),state(2,2,1,1,left),state(2,0,1,3,right),state(3,0,0,3,left),state(1,0,2,3,right),state(2,0,1,3,left),state(0,0,3,3,right)] ? ;
+P = [state(3,3,0,0,left),state(1,3,2,0,right),state(2,3,1,0,left),state(0,3,3,0,right),state(1,3,2,0,left),state(1,1,2,2,right),state(2,2,1,1,left),state(2,0,1,3,right),state(3,0,0,3,left),state(1,0,2,3,right),state(1,1,2,2,left),state(0,0,3,3,right)] ? ;
+P = [state(3,3,0,0,left),state(2,2,1,1,right),state(2,3,1,0,left),state(0,3,3,0,right),state(1,3,2,0,left),state(1,1,2,2,right),state(2,2,1,1,left),state(2,0,1,3,right),state(3,0,0,3,left),state(1,0,2,3,right),state(2,0,1,3,left),state(0,0,3,3,right)] ? ;
+P = [state(3,3,0,0,left),state(2,2,1,1,right),state(2,3,1,0,left),state(0,3,3,0,right),state(1,3,2,0,left),state(1,1,2,2,right),state(2,2,1,1,left),state(2,0,1,3,right),state(3,0,0,3,left),state(1,0,2,3,right),state(1,1,2,2,left),state(0,0,3,3,right)] ? ;
+no
+
+
+| ?- bf_search(P).
+P = [state(3,3,0,0,left),state(1,3,2,0,right),state(2,3,1,0,left),state(0,3,3,0,right),state(1,3,2,0,left),state(1,1,2,2,right),state(2,2,1,1,left),state(2,0,1,3,right),state(3,0,0,3,left),state(1,0,2,3,right),state(2,0,1,3,left),state(0,0,3,3,right)] ? ;
+P = [state(3,3,0,0,left),state(1,3,2,0,right),state(2,3,1,0,left),state(0,3,3,0,right),state(1,3,2,0,left),state(1,1,2,2,right),state(2,2,1,1,left),state(2,0,1,3,right),state(3,0,0,3,left),state(1,0,2,3,right),state(1,1,2,2,left),state(0,0,3,3,right)] ? ;
+P = [state(3,3,0,0,left),state(2,2,1,1,right),state(2,3,1,0,left),state(0,3,3,0,right),state(1,3,2,0,left),state(1,1,2,2,right),state(2,2,1,1,left),state(2,0,1,3,right),state(3,0,0,3,left),state(1,0,2,3,right),state(2,0,1,3,left),state(0,0,3,3,right)] ? ;
+P = [state(3,3,0,0,left),state(2,2,1,1,right),state(2,3,1,0,left),state(0,3,3,0,right),state(1,3,2,0,left),state(1,1,2,2,right),state(2,2,1,1,left),state(2,0,1,3,right),state(3,0,0,3,left),state(1,0,2,3,right),state(1,1,2,2,left),state(0,0,3,3,right)] ? ;
+no
+
+
+
+
+*/

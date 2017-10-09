66de8345dc5f246b4fd5a382d32b49b39992db37
Fredrik Wallström <frewa814@student.liu.se>
2017-10-09 16:17:32 +0200

    Removed the cuts from union and intersection predicates

--- Lab2/e2_4.pl
+++ Lab2/e2_4.pl
@@ -4,46 +4,32 @@
 % Res is the result you get if you take the union of the sets S1 and S2.
 union([], [], []) . 
 union([S1|T1], [], [S1|Res]) :-
-	union(T1, [], Res),
-	!.
+	union(T1, [], Res).
 union([], [S2|T2], [S2|Res]) :-
-	union([], T2, Res),
-	!.
+	union([], T2, Res).
 union([S1|T1], T2, Res) :-
 	member(S1, T2),
-	union(T1, T2, Res),
-	!.
+	union(T1, T2, Res).
 union([S1|T1], [S2|T2], [S1|Res]) :-
 	nonmember(S1, [S2|T2]),
 	S1 @=< S2,
-	union(T1, [S2|T2],  Res),
-	!.
+	union(T1, [S2|T2],  Res).
 union([S1|T1], [S2|T2], [S2|Res]) :-
 	nonmember(S1, [S2|T2]),
 	S2 @=< S1,
-	union([S1|T1], T2,  Res),
-	!.
+	union([S1|T1], T2,  Res).
 
 % intersection(S1, S2, Res)
 % Res is the intersection of the sets S1 and S2.
 intersection(S1, S2, Res) :-
 	qsort(S1, S1Sorted),
-	qsort(S2, S2Sorted),
-	inters(S1Sorted, S2Sorted, Res).
-inters([], [], []) :-
-	!.
-inters([], _, []) :-
-	!.
-inters(_, [], []) :-
-	!.
-inters([H1|S1],  [H2|S2], Res) :-
-	H1 @< H2,
-	inters(S1, [H2|S2], Res).
-inters([H1|S1], [H2|S2], Res) :-
-	H2 @< H1,
-	inters([H1|S1], S2, Res).
-inters([H1|S1], [H2|S2], [H1|Res]) :-
-	H2 = H1,
+	inters(S1Sorted, S2, Res).
+inters([], _, []).
+inters([H1|S1],  S2, [H1|Res]) :-
+	once(member(H1, S2)),
+	inters(S1, S2, Res).
+inters([H1|S1], S2, Res) :-
+	nonmember(H1, S2),
 	inters(S1, S2, Res).
 
 % split(L, Pivot, L1, L2).
@@ -115,6 +101,9 @@ Res = [a] ? ;
 no
 | ?- intersection([], [a,b,c,d,e,f,g,h], Res).
 Res = [] ? ;
+no
+| ?- intersection([b,c,a], [a,f,g,h,a,b,c,i,c,c,c,c,c,c,c], Res).
+Res = [a,b,c] ? ;
 no
 | ?- powerset([b,a,c], Res).
 Res = [[a,b,c],[a,b],[a,c],[a],[b,c],[b],[c],[]] ? ;
